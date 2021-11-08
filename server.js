c4538db81057d7d03d63b9de6cc82fd198ac3b9c
axelc <tailor.1999agcm@gmail.com>
2021-11-08 12:31:49 -0600

    Añadiendo acceso al cors

--- server.js
+++ server.js
@@ -12,3 +12,11 @@ app.get('/*', function(req, res) {
 
 // default Heroku port
 app.listen(process.env.PORT || 4200);
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
